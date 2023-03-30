89b5f21ddb601ced2c967f35e21ec078dba44d84
aleksvvv <adm-shop-001@yandex.ru>
2023-03-30 10:25:43 +0300

    Use ViewBinding in Adapter

---
+++ app/src/main/java/com/bignerdranch/android/cryptocoin/presentation/adapter/CoinInfoViewHolder.kt
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.cryptocoin.presentation.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.bignerdranch.android.cryptocoin.databinding.ItemCoinInfoBinding
+
+class CoinInfoViewHolder(
+    val binding: ItemCoinInfoBinding
+) :
+    RecyclerView.ViewHolder(binding.root)
